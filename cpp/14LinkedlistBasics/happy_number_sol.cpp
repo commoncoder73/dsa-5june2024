7ccf4c7992c23b6b9ea64758f5f5c874e6d7ae69
commoncoder73 <commoncoder73@gmail.com>
2024-06-21 13:51:46 +0530

    Make formatting changes for cpp files using Google cpp style formatter

--- cpp/14LinkedlistBasics/happy_number_sol.cpp
+++ cpp/14LinkedlistBasics/happy_number_sol.cpp
@@ -2,28 +2,25 @@
 
 using namespace std;
 
-int sumOfSqaureOfDigits(int n)
-{
-    int sum = 0;
-    while (n > 0)
-    {
-        int rem = n % 10;
-        sum += (rem * rem);
-        n /= 10;
-    }
-    return sum;
+int sumOfSqaureOfDigits(int n) {
+  int sum = 0;
+  while (n > 0) {
+    int rem = n % 10;
+    sum += (rem * rem);
+    n /= 10;
+  }
+  return sum;
 }
 
-bool isHappyNumber(int n)
-{
-    int slow = n;
-    int fast = n;
-    while(fast != 1) {
-        slow = sumOfSqaureOfDigits(slow);
-        fast = sumOfSqaureOfDigits(sumOfSqaureOfDigits(fast));
-        if (slow == fast) {
-            return false;
-        }
-    }   
-    return true;
+bool isHappyNumber(int n) {
+  int slow = n;
+  int fast = n;
+  while (fast != 1) {
+    slow = sumOfSqaureOfDigits(slow);
+    fast = sumOfSqaureOfDigits(sumOfSqaureOfDigits(fast));
+    if (slow == fast) {
+      return false;
+    }
+  }
+  return true;
 }
